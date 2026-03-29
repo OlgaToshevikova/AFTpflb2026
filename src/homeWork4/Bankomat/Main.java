ce64bfcd79ae6807d917fd7c0d426a3f91b50af0
OlgaToshevikova <ostoshevikova@gmail.com>
2026-03-29 18:49:53 +0400

    Add HomeWork_4

--- src/homeWork4/Bankomat/Main.java
+++ src/homeWork4/Bankomat/Main.java
@@ -2,9 +2,8 @@ package homeWork4.Bankomat;
 
 public class Main {
     public static void main(String[] args) {
-        Bankomat bankomat=new Bankomat(0,0,0);
-
-        bankomat.addMoney(20,2,1);
-        bankomat.withdrawMoney(200);
+        Bankomat_1 bankomat1=new Bankomat_1(10,10,10);
+        bankomat1.addMoney(20,2,2);
+       bankomat1.withdrawMoney(280);
     }
 }
